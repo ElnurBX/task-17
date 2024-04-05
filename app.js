698ff5538c54c81803b7df6a4157d87f4f7794a0
ElnurBX <tu8dhdtwy@code.edu.az>
2024-04-05 22:57:45 +0400

    1.0.0

---
+++ app.js
@@ -0,0 +1,36 @@
+//* task1
+// function bank(miqdar,ay){
+//     arr3=[]
+//     let son
+//     let qaliqmebleq= miqdar%ay
+//     let aylara_bolunen=(miqdar-qaliqmebleq)/ay
+//     for (let i = 0; i <ay; i++) {
+//         arr3[i]=aylara_bolunen
+//     }
+//     son=arr3[arr3.length-1]+qaliqmebleq
+//     arr3[arr3.length-1]=son  
+//     for (let i = 0; i <ay; i++) {
+//         console.log(`${i+1}-ci ay ${arr3[i]}`)
+//     }
+//     return arr3
+// }
+// console.log(bank(1244,5))
+// *task2
+// array=[1,2,3,4,4]
+// function dublicate(array){
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j =i+1; j < array.length; j++) {
+//             if(array[i]==array[j]){
+//                 console.log(`dublicte${array[i]}`);
+//             }
+//         }
+//     }
+// }
+// dublicate(array)
+//*task 3
+// function toCelsiusa(f){
+//     let x
+//     x=(f*(9/5))+32
+//     return x
+// }
+// console.log(toCelsiusa(10));
